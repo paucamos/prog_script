53bf6a68534a2c276dd51f5c90199218fa32d101
paucamos <paucapaofficial@gmail.com>
2022-05-31 09:31:31 +0000

    modifications and python requirements

--- a.sh
+++ a.sh
@@ -6,12 +6,13 @@
 # Testing
 #rm download
 #rm sample.csv
+#rm raw_data.csv
 
 # Main script
-ID="https://opendata-ajuntament.barcelona.cat/data/dataset/15b349cd-3d4d-4a62-9ad3-d67230029a23/resource/3224123d-488e-4e03-9b64-ed278a9c3c79/download" 
-curl -o raw_data.csv https://drive.google.com/uc?export=download&id=1jRvPwX7SzXI1o1ZLWyu2bXQuMF7ZDtvG
+URL="https://raw.githubusercontent.com/paucamos/prog_script/main/raw_data.csv" 
+wget -nv $URL
 
-# Si l'script s'executa sense opcions, mostra la ID de descàrrega i el nombre de
+# Si l'script s'executa sense opcions, mostra la URL de descàrrega i el nombre de
 # columnes i registres. 
 
 # Si s'inclou en la seva invocació l'opció -v, a més,
@@ -19,18 +20,18 @@ curl -o raw_data.csv https://drive.google.com/uc?export=download&id=1jRvPwX7SzXI
 
 # Opcio sense -v
 if [ -z "$1" ]; then
-    echo "Numero de registres: $(wc -l < download)"
-    echo "Numero de columnes: $(head -1 download | sed 's/[^,]//g' | wc -c)"
+    echo "Numero de registres: $(wc -l < raw_data.csv)"
+    echo "Numero de columnes: $(head -1 raw_data.csv | sed 's/[^,]//g' | wc -c)"
 
 # Opcio amb -v
 else
-    echo "Numero de registres: $(wc -l < download)"
-    echo "Numero de columnes: $(head -1 download | sed 's/[^,]//g' | wc -c)"
-    echo "Tipus de fitxer: $(file -ib download)"
+    echo "Numero de registres: $(wc -l < raw_data.csv)"
+    echo "Numero de columnes: $(head -1 raw_data.csv | sed 's/[^,]//g' | wc -c)"
+    echo "Tipus de fitxer: $(file -ib raw_data.csv)"
     echo "Data types columnes:"
 
     # Per mostrar les dades nomes necesitem una linea de dades
-    sed -n 2p download > sample.csv
+    sed -n 2p raw_data.csv > sample.csv
 
     # Delimitador
     IFS=","
