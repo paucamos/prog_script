0bc27cb30e0423ea31ef1a25e81e20a0cc79460c
paucamos <paucapaofficial@gmail.com>
2022-06-05 20:11:29 +0000

    last modifications

--- del.sh
+++ del.sh
@@ -1,6 +1,6 @@
 # Script per borrar les dades
-(cd 'scripts/A' ; rm raw_data.csv)
-(cd 'scripts/B' ; rm b0.csv)
-(cd 'scripts/B' ; rm b1.csv)
-(cd 'scripts/B' ; rm b2.csv)
+(cd './' ; rm raw_data.csv)
+(cd './' ; rm b0.csv)
+(cd './' ; rm b1.csv)
+(cd './' ; rm b2.csv)
 (cd './' ; rm index.html)
--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 start=`date +%s`
 
 # Script A
-(cd 'scripts/A' ; ./a.sh [-v])
+(cd './' ; ./a.sh [-v])
 
 # Scripts B
-(cd 'scripts/B' ; gawk -f b.awk '../A/raw_data.csv')
-(cd 'scripts/B' ; ./b.sh 'b0.csv')
-(cd 'scripts/B' ; gawk -f b1.awk 'b1.csv')
+(cd './' ; gawk -f b.awk 'raw_data.csv')
+(cd './' ; ./b.sh 'b0.csv')
+(cd './' ; gawk -f b1.awk 'b1.csv')
 
 # Script C
-(cd 'scripts/C' ; gawk -f c.awk '../B/b2.csv') > index.html
+(cd './' ; gawk -f c.awk 'b2.csv') > index.html
 end=`date +%s`
 runtime=$((end-start))
 
